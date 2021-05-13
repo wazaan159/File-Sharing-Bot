11a93c211a3ad43a0220f5654ec0d2c182130643
wazaan159 <84114353+wazaan159@users.noreply.github.com>
2021-05-13 21:09:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @Cinema_Festival\n○ Group : @CF_OfficialGroup</b>",
+            text = f"<b>○ Creator : <a href='Cinema_FestivalTG'>This Channel</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ FB Page : <a href='https://www.facebook.com/CinemaFestivalOfficial'>Click here</a>\n○ Channel : @Cinema_FestivalTG\n○ Group : @CF_OfficialGroup</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
